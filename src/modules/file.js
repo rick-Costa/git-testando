baf73a01d13df20a598b3f0caee9c6ae48c5e56e
Rick-Costa <erickcosta33.ec33@gmail.com>
2023-01-03 02:08:35 -0300

    implementando funcionalidade join e a utilizando em file.js

--- src/modules/file.js
+++ src/modules/file.js
@@ -1,6 +1,12 @@
-import { path } from './path';
+import { path, join } from './path';
 
 export function main(full_path, options = { source_file: '' }) {
-        const resove = path(full_path);
+        let resolve;
+
+        if (join(options.source_file, '/file/arquivos.txt')) {
+                resolve = path(full_path, options);
+        }
+
+        return resolve;
 
 }
--- src/modules/path.js
+++ src/modules/path.js
@@ -5,3 +5,11 @@ export function path(full_path, options = { source_file: '' }) {
 
         return path_resolve;
 }
+
+export function join(...paths) {
+        const path_joinded = paths.reduce(function (previous, current) {
+                return (previous += current);
+        }, "");
+
+        return path_joinded;
+}
