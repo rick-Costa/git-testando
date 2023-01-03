249ad9df608da655d64a578dea9535a8baf4eaaf
Rick-Costa <erickcosta33.ec33@gmail.com>
2023-01-03 15:44:42 -0300

    criando metodo dummy em file.js

--- src/modules/file.js
+++ src/modules/file.js
@@ -10,3 +10,13 @@ export function main(full_path, options = { source_file: '' }) {
         return resolve;
 
 }
+
+export function dummy(full_path, options = { source_file: '' }) {
+        let resolve
+
+        if (join(options.source_file, '/file/arquivos.txt')) {
+                resolve = path(full_path, options)
+        }
+
+        return resolve
+}
