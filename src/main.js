95bb64d4575fd5c4ced27213480045dc46e50b29
Rick-Costa <erickcosta33.ec33@gmail.com>
2023-01-03 16:46:57 -0300

    fazendo request no main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { main } from './modules/file.js';
+import Request from './modules/request.js';
 
 const options = { source_file: 'C:/ProgramFiles/nodejs/corepack' }
 
 const resolve = main('C:/ProgramFiles/nodejs/corepack', options);
+
+const request = new Request('C:/ProgramFiles/nodejs/corepack', resolve);
+request.init();
